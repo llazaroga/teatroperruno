da9fc2a8421eacdca771a7feeeb5acf9e523f447
Lucía Lázaro <lucia@MacBook-Air-de-Lucia.local>
2021-12-03 22:10:25 +0100

    First commit

---
+++ src/assets/scripts/main.js
@@ -0,0 +1,15 @@
+/**
+ * Import dependencies from node_modules
+ * see commented examples below
+ */
+
+import * as bootstrap from 'bootstrap';
+
+/**
+ * Write any other JavaScript below
+ */
+
++(function () {
+  const university = "UOC";
+  console.log(`Hello, ${university}!`);
+})();
